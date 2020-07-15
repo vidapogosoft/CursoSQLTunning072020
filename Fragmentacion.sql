a9f4ff62dec0751997fa22681f6cb08bb4b8c588
vidapogosoft <vidapogosoft@hotmail.com>
2020-07-15 18:05:07 -0500

    Taller dia 1
    index

---
+++ Fragmentacion.sql
@@ -0,0 +1,11 @@
+
+--Fragmentation
+SELECT OBJECT_NAME(ix.object_ID) AS TableName, 
+       ix.name AS IndexName, 
+       ixs.index_type_desc AS IndexType, 
+       ixs.avg_fragmentation_in_percent
+FROM sys.dm_db_index_physical_stats(DB_ID(), NULL, NULL, NULL, NULL) ixs
+     INNER JOIN sys.indexes ix ON ix.object_id = ixs.object_id
+                                  AND ixs.index_id = ixs.index_id
+WHERE ixs.avg_fragmentation_in_percent >= 30
+ORDER BY ixs.avg_fragmentation_in_percent DESC;
